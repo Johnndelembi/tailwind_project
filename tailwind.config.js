a7d0dbad4db3a5aca51f57f21990f4729f500020
John <williamjohnie61@gmail.com>
2024-08-23 18:01:24 +0300

    hot fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,30 @@ module.exports = {
       xl: '1440px',
     },
     extend: {
+      animation: {
+        tilt: 'tilt 10s infinite linear',
+      },
+      keyframes: {
+        tilt: {
+          "0% 50% 100%": {
+            transform: "rotate(0deg)",
+          },
+
+          "25%": {
+            transform: "rotate(2deg)",
+          },
+
+          "75%": {
+            transform: "rotate(-2deg)",
+          },
+
       colors: {
         // you can write your custom colors in here .ie brightRed: '',
       }
     },
   },
   plugins: [],
+}
+  },
 }
 
